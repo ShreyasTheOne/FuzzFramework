efcced577edaf5ac22ceda3d6075b0bd7a140c84
Shreyas Dodamani <shreyurd@gmail.com>
2022-02-19 10:14:56 +0530

    Add method to send request and log details to file

--- core/sample_fuzzer/__init__.py
+++ core/sample_fuzzer/__init__.py
@@ -1,13 +1,13 @@
 from api_interface import api_configuration
+from api_interface.request_engine import RequestEngine
 from core.sample_fuzzer.data_generators import *
 
 class SampleFuzzer:
 
     def __init__(self):
         self.API_CONFIGURATION = api_configuration.API_CONFIGURATION.structure
-    
-        print(IntGenerator().generate())
-        print(StrGenerator().generate())
-        print(BoolGenerator().generate())
-        print(ListGenerator().generate())
-        print(DictGenerator().generate())
+        requestEngine = RequestEngine('create_instant_meeting')
+        requestEngine.send_request()
+        requestEngine.send_request()
+        requestEngine.send_request()
+        requestEngine.send_request()
